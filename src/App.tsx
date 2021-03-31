df9f789b44dd59431de91d0d20bd1a2ec5596094
Tiaguin061 <tiaguin180@gmail.com>
2021-03-31 16:35:16 -0300

    refactor: Alter Component for adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import Login from './pages/login';
 
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './hooks/AuthContext';
+import Routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const App: React.FC = () => {
   return (
-    <>
-      <Login />
+    <Router>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
